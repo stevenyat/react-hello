3552be335202ca6feb570e29b62da99d286d3217
Steven Acero <stevenacero07@gmail.com>
2025-12-02 03:50:21 +0000

    clases

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+const Home = () => {
+	
+	const [task, setTask] = useState("");
+	const [taskList, setTaskList] = useState ([]);
+
+
+		return (
+		<div className="d-flex justify-content-center align-items-center flex-column">
+			<h1>TODOS</h1>
+			<input 
+				type="text"
+				placeholder="What needs to be done?"
+				className="form-control w-25 mx-auto"
+				onChange={(e) => setTask (e.target.value)}
+				onKeyUp={(e) => {
+					if (e.key === "Enter") {
+						setTaskList([...taskList, task]);
+						setTask("");
+					}
+				}}
+			/>
+			{/* <button onClick={() => setTaskList([...taskList, task])} className="btn btn-primary my-2">
+				Add Task
+			</button> */}
+			{taskList.length === 0 && <p className="mt-3">No hay tareas, añadir tareas</p>}
+			<ul>
+				{taskList.map((task, index) => <li key={index}>{task}
+					<button onClick={()=> setTaskList(taskList.filter((item,ind)=> ind !== index)) }
+						className="btn-close"></button> 
+					</li>)}
+			</ul>
 		</div>
 	);
 };
